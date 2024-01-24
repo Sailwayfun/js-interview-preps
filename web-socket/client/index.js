3040e21b6c28a71550849f71456ffd5a8c28a93f
Sailwayfun <liaoleon000513@gmail.com>
2024-01-24 10:41:23 +0800

    refactor: wrap socket functions in an object

--- web-socket/client/index.js
+++ web-socket/client/index.js
@@ -1,33 +1,40 @@
-let ws;
-
-function connect() {
-    ws = new WebSocket('ws://localhost:9000');
-    ws.onopen = () => {
-        console.log('[Client] Connected to server');
-    };
-    ws.onmessage = message => {
-        // console.log({ message });
-        console.log(`[Client] Received: ${message.data}`);
-        document.getElementById('server-msg').textContent = message.data;
-    };
-}
-
-function disconnect() {
-    ws.close();
-    ws.onclose = () => {
-        console.log('[Client] Disconnected from server');
-    };
-}
-
-function sendMessageToServer() {
-    const message = document.getElementById('message').value;
-    if (ws) {
-        ws.send(message);
-        return;
+function createClient() {
+    let ws;
+    function connect() {
+        ws = new WebSocket('ws://localhost:9000');
+        ws.onopen = () => {
+            console.log('[Client] Connected to server');
+        };
+        ws.onmessage = message => {
+            // console.log({ message });
+            console.log(`[Client] Received: ${message.data}`);
+            document.getElementById('server-msg').textContent = message.data;
+        };
     }
-    console.log('[Client] Not connected to server');
 
+    function disconnect() {
+        ws.close();
+        ws.onclose = () => {
+            console.log('[Client] Disconnected from server');
+        };
+    }
+
+    function sendMessageToServer() {
+        const message = document.getElementById('message').value;
+        if (ws) {
+            ws.send(message);
+            return;
+        }
+        console.log('[Client] Not connected to server');
+    }
+
+    return {
+        connect,
+        disconnect,
+        sendMessageToServer
+    };
 }
+const { connect, disconnect, sendMessageToServer } = createClient();
 
 document.getElementById('connect').addEventListener('click', connect);
 document.getElementById('disconnect').addEventListener('click', disconnect);
