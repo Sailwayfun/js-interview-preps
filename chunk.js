7afd9f234a9f0dcf902185cc55d107d845c2cf19
Sailwayfun <liaoleon000513@gmail.com>
2024-01-29 16:23:42 +0800

    implement chunkArray() function

--- chunk.js
+++ chunk.js
@@ -4,17 +4,26 @@
 // // 輸出: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 // const chunkedArray = chunkArray(originalArray, chunkSize);
 
+// function chunkArray(originalArray, chunkSize) {
+//     let result = [];
+//     const length = originalArray.length;
+//     for (let i = 0; i < length; i++) {
+//         result.push(originalArray.splice(0, chunkSize));
+//     }
+//     result = result.filter(arr => arr.length != 0);
+//     return result;
+// }
+
 function chunkArray(originalArray, chunkSize) {
     let result = [];
-    for (let i = 0; i < chunkSize; i++) {
-        let inner = [];
-        for (let j = 0; j < originalArray.length / chunkSize; j++) {
-            inner.push(originalArray[j]);
-        }
-        result.push(inner);
+    const length = originalArray.length;
+    for (let i = 0; i < length; i += chunkSize) {
+        result.push(originalArray.slice(i, i + chunkSize));
     }
     return result;
 }
 
 const chunkedArray = chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9], 3);
+const chunkedArray2 = chunkArray([-1, 2, 5, 6, 8, 9, 11, 567], 2);
 console.log(chunkedArray);
+console.log(chunkedArray2);
