26a18cacaeac493c9ab4487a84f741790671315e
Sailwayfun <liaoleon000513@gmail.com>
2024-02-14 08:36:21 +0800

    change conditional flow

--- majority-element.js
+++ majority-element.js
@@ -1,18 +1,26 @@
 function findMajorityElement(arr) {
     ///Boyer–Moore majority vote algorithm
     let result = arr[0];
-    let count = 0;
+    let count = 1;
     arr.forEach(element => {
-        if (element === result) {
-            count++;
-        } else if (count > 0) {
-            count--;
-        } else {
+        // if (element === result) {
+        //     count++;
+        // } else if (count > 0) {
+        //     count--;
+        // } else {
+        //     result = element;
+        //     count++;
+        // }
+        if (count === 0 && element !== result) {
             result = element;
             count++;
+        } else if (count > 0) {
+            count++;
+        } else {
+            count--;
         }
     });
     return result;
 }
 
-console.log(findMajorityElement([3, 3, 3, 1, 3, 2, 2, 3, 1, 1, 2, 2, 2]));
+console.log(findMajorityElement([3, 3, 3, 3, 1, 3, 2, 2, 3, 1, 1, 2, 2, 2]));
